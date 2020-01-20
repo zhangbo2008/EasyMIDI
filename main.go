3772bb076560c6e7fbb385425326f878b565f17b
zhangbo2008 <15122306087@163.com>
2020-01-21 01:35:42 +0800

    1

---
+++ main.go
@@ -0,0 +1,14 @@
+package main
+
+
+//写下如何调用
+
+//下载别人的代码,然后把他clone到goroot/src里面才行,才能调用!
+//因为别人的import 都是github.com/作者这么写的.所以也在自己的上面src目录里面放这个目录才行.
+//dsfdsfsdf
+import "EasyMIDI/uu"
+
+func main(){
+	print("dfdsfasadf")
+	print(uu.A)
+}
--- smf/division_test.go
+++ smf/division_test.go
@@ -1,5 +1,11 @@
 package smf
 
+///////////////////////////////
+//写下如何调用
+
+//下载别人的代码,然后把他clone到goroot/src里面才行,才能调用!
+//因为别人的import 都是github.com/作者这么写的.所以也在自己的上面src目录里面放这个目录才行.
+//dsfdsfsdf
 import "testing"
 
 func TestCheckSMTPE(t *testing.T) {
